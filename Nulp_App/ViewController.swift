b55760c7e4165faa49bd84a944d014b0d4392055
i1 <ivansapiga1999@gmail.com>
2018-09-02 22:43:56 +0300

    [+] add 1 lab

--- Nulp_App/ViewController.swift
+++ Nulp_App/ViewController.swift
@@ -9,18 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var Label: UILabel!
-    @IBOutlet weak var TextField: UITextField!
-    @IBAction func sayButton(_ sender: UIButton) {
-        if (TextField.text?.isEmpty)! {
-                    Label.text = "Hello Friend"
-        }else{
-            Label.text = "Hello \(String(describing: TextField.text!))"
-        }
-    }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("Hello World:)")
     }
 }
 
