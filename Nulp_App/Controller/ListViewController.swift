9af8672162435a6d947f4127e77951a4cba4cd5c
i1 <ivansapiga1999@gmail.com>
2018-09-24 20:59:05 +0300

    [+]finish 4 lab

--- Nulp_App/Controller/ListViewController.swift
+++ Nulp_App/Controller/ListViewController.swift
@@ -8,10 +8,11 @@
 
 import UIKit
 
-class TableVCViewController: UIViewController {
+class ListViewController: UIViewController {
     
-    @IBOutlet weak var tableView: UITableView!
-
+    @IBOutlet fileprivate weak var tableView: UITableView!
+    
+    // MARK: - LifeCycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,17 +24,17 @@ class TableVCViewController: UIViewController {
 
 // MARK: - UITableViewDataSource
 
-extension TableVCViewController: UITableViewDataSource {
+extension ListViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        let placesArray = ViewController.getPlaces()
+        let placesArray = PersonManager.getPersons()
         return placesArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "TableTableViewCell", for: indexPath) as! TableTableViewCell
+        let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: PersonTableViewCell.self), for: indexPath) as! PersonTableViewCell
         
-        let placesArray = ViewController.getPlaces()
+        let placesArray = PersonManager.getPersons()
         let person = placesArray[indexPath.row]
         cell.configureWith(person: person)
         
@@ -44,7 +45,7 @@ extension TableVCViewController: UITableViewDataSource {
 
 // MARK: - UITableViewDelegate
 
-extension TableVCViewController: UITableViewDelegate {
+extension ListViewController: UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 150.0
